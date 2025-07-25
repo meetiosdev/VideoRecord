308d460df99eac065d512ba905382a8ba6eaed62
Swarajmeet Singh <swarajmeet.crebos@gmail.com>
2025-07-25 11:07:04 +0530

    Initial Commit

---
+++ VideoRecord/VideoRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoRecordApp.swift
+//  VideoRecord
+//
+//  Created by Swarajmeet Singh on 25/07/25.
+//
+
+import SwiftUI
+
+@main
+struct VideoRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
